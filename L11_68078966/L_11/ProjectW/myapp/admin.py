7e7bbff438e6a3d5bdebaae636388631fd540a0e
kittipojpho-stack <kittipoj.pho@spumail.net>
2026-03-31 17:04:03 +0700

    Delete L11_68078966/L_11/ProjectW directory

--- L11_68078966/L_11/ProjectW/myapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from myapp.models import Person
-
-# Register your models here.
-admin.site.register(Person)
--- L11_68078966/L_11/ProjectW/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from myapp import views
-urlpatterns = [
-    path('',views.index),
-    path('about',views.about),
-    path('form',views.form),
-    path('addData', views.addData),
-]
